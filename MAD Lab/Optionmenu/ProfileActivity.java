db349a65114bb7d77660a289fcf2085337e32fc4
AswathiManoj0 <aswathi4882@gmail.com>
2025-09-24 11:45:42 +0530

    Create ProfileActivity.java

---
+++ MAD Lab/Optionmenu/ProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.optionmenu;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.option.R;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
