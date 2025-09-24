c8c056c73d274950a09fd0275e74193685fdbc2a
AswathiManoj0 <aswathi4882@gmail.com>
2025-09-24 11:46:55 +0530

    Create SettingsActivity.java

---
+++ MAD Lab/Optionmenu/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.optionmenu;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.option.R;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
